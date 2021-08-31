00b7baeb892d5a5376fda2d121582a0f5ec87f61
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-08-31 23:18:27 +0300

    Create books.sql

---
+++ k8s/books.sql
@@ -0,0 +1,14 @@
+create databese books;
+
+use database books;
+
+CREATE TABLE books( 
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title CHAR(100) NOT NULL,
+author CHAR(40) NOT NULL,
+price INT NOT NULL
+);
+
+INSERT INTO books (title,author,price) VALUES("Green Mile","Stephen King","17");
+
+INSERT INTO books (title,author,price) VALUES("The Casal Vacancy", "J.K.Rowling", "23");
